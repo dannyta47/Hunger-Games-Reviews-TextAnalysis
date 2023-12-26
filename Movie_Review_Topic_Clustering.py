673de5c7ccd24c623ab7ae6e6b7149c122cc62cd
Danny Ta <140979642+dannyta47@users.noreply.github.com>
2023-12-25 19:07:33 -0800

    Add files via upload

---
+++ Movie_Review_Topic_Clustering.py
@@ -0,0 +1,167 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Oct 16 22:16:29 2023
+
+@author: easonwarren
+"""
+
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Oct 16 21:48:59 2023
+
+@author: easonwarren
+"""
+import sys
+# !{sys.executable} -m spacy download en
+import re, numpy as np, pandas as pd
+from pprint import pprint
+
+# Gensim
+#pip install genism.utils
+import gensim, spacy, logging, warnings
+import gensim.corpora as corpora
+#from gensim.utils import lemmatize, simple_preprocess
+from gensim.models import CoherenceModel
+import matplotlib.pyplot as plt
+
+# NLTK Stop words
+from nltk.corpus import stopwords
+stop_words = stopwords.words('english')
+stop_words.extend(['from', 'subject', 're', 'edu', 'use', 'not', 'would', 'say', 'could', '_', 'be', 'know', 'good', 'go', 'get', 'do', 'done', 'try', 'many', 'some', 'nice', 'thank', 'think', 'see', 'rather', 'easy', 'easily', 'lot', 'lack', 'make', 'want', 'seem', 'run', 'need', 'even', 'right', 'line', 'even', 'also', 'may', 'take', 'come'])
+
+%matplotlib inline
+#warnings.filterwarnings("ignore",category=DeprecationWarning)
+#logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.ERROR)
+
+import pandas as pd 
+import random 
+
+
+## Movie
+
+reviews_movie = pd.read_csv("/Users/easonwarren/Downloads/MSA/Text Analytics/IMDBHungerGamesReviews (1).csv")
+
+# Randomly selecting the same number as the book revies
+random.seed(1234)
+random_500 = random.sample(range(len(reviews_movie)), 595)
+
+# Grabs the random sample of rows from the Hunger Games Movie Review
+reviews_movie = reviews_movie.iloc[random_500]
+
+reviews = reviews_movie['Review']
+
+review_list = reviews.tolist()
+len(review_list)
+        
+        
+## START OF ANALYSIS
+
+import re
+
+
+def sent_to_words(sentences):
+    for sent in sentences:
+        sent = re.sub('\S*@\S*\s?', '', sent)  # remove emails
+        sent = re.sub('\s+', ' ', sent)  # remove newline chars
+        sent = re.sub("\'", "", sent)  # remove single quotes
+        sent = gensim.utils.simple_preprocess(str(sent), deacc=True) 
+        yield(sent)  
+
+# Convert to list
+#data = df.content.values.tolist()
+data_words = list(sent_to_words(review_list))
+print(data_words[:2])
+
+
+
+from gensim.utils import simple_preprocess
+
+# Build the bigram and trigram models
+bigram = gensim.models.Phrases(data_words, min_count=5, threshold=100) # higher threshold fewer phrases.
+trigram = gensim.models.Phrases(bigram[data_words], threshold=100)  
+bigram_mod = gensim.models.phrases.Phraser(bigram)
+trigram_mod = gensim.models.phrases.Phraser(trigram)
+
+# !python3 -m spacy download en  # run in terminal once
+def process_words(texts, stop_words=stop_words, allowed_postags=['NOUN', 'ADJ', 'VERB', 'ADV']):
+    """Remove Stopwords, Form Bigrams, Trigrams and Lemmatization"""
+    texts = [[word for word in simple_preprocess(str(doc)) if word not in stop_words] for doc in texts]
+    texts = [bigram_mod[doc] for doc in texts]
+    texts = [trigram_mod[bigram_mod[doc]] for doc in texts]
+    texts_out = []
+    nlp = spacy.load('en_core_web_sm', disable=['parser', 'ner'])
+    for sent in texts:
+        doc = nlp(" ".join(sent)) 
+        texts_out.append([token.lemma_ for token in doc if token.pos_ in allowed_postags])
+    # remove stopwords once more after lemmatization
+    texts_out = [[word for word in simple_preprocess(str(doc)) if word not in stop_words] for doc in texts_out]    
+    return texts_out
+
+data_ready = process_words(data_words)  # processed Text Data!
+     
+
+
+
+# Create Dictionary
+id2word = corpora.Dictionary(data_ready)
+
+# Create Corpus: Term Document Frequency
+corpus = [id2word.doc2bow(text) for text in data_ready]
+
+# Build LDA model
+lda_model = gensim.models.ldamodel.LdaModel(corpus=corpus,
+                                           id2word=id2word,
+                                           num_topics=4, 
+                                           random_state=100,
+                                           update_every=1,
+                                           chunksize=10,
+                                           passes=10,
+                                           alpha='symmetric',
+                                           iterations=100,
+                                           per_word_topics=True)
+
+pprint(lda_model.print_topics())
+        
+        
+        
+        
+        
+def format_topics_sentences(ldamodel=None, corpus=corpus, texts=review_list):
+    # Init output
+    sent_topics_df = pd.DataFrame()
+
+    # Get main topic in each document
+    for i, row_list in enumerate(ldamodel[corpus]):
+        row = row_list[0] if ldamodel.per_word_topics else row_list            
+        # print(row)
+        row = sorted(row, key=lambda x: (x[1]), reverse=True)
+        # Get the Dominant topic, Perc Contribution and Keywords for each document
+        for j, (topic_num, prop_topic) in enumerate(row):
+            if j == 0:  # => dominant topic
+                wp = ldamodel.show_topic(topic_num)
+                topic_keywords = ", ".join([word for word, prop in wp])
+                sent_topics_df = pd.concat([sent_topics_df, pd.DataFrame([[int(topic_num), round(prop_topic,4), topic_keywords]])], ignore_index = True)
+            else:
+                break
+    sent_topics_df.columns = ['Dominant_Topic', 'Perc_Contribution', 'Topic_Keywords']
+
+    # Add original text to the end of the output
+    contents = pd.Series(texts)
+    sent_topics_df = pd.concat([sent_topics_df, contents], axis=1)
+    return(sent_topics_df)
+
+
+df_topic_sents_keywords = format_topics_sentences(ldamodel=lda_model, corpus=corpus, texts=data_ready)
+
+print(df_topic_sents_keywords.head(12))
+
+# Format
+df_dominant_topic = df_topic_sents_keywords.reset_index()
+df_dominant_topic.columns = ['Document_No', 'Dominant_Topic', 'Topic_Perc_Contrib', 'Keywords', 'Text']
+df_dominant_topic.head(12)
+
+
+df_dominant_topic.to_csv("topic_clustering_book_english.csv", index = False)
+
